61a9810ca5493d441d7cd98872b3a9377c457d77
Joseph Choi <robocup30@hotmail.com>
2016-11-02 20:48:52 -0700

    Window selecting

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -25,6 +26,9 @@ namespace AutoClicker
     {
         [DllImport("user32.dll")]
         static extern IntPtr WindowFromPoint(int xPoint, int yPoint);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(POINT Point);
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, IntPtr windowTitle);
@@ -55,6 +59,10 @@ namespace AutoClicker
         internal static extern bool GetCursorPos(ref Win32Point pt);
 
         [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
         static extern IntPtr GetDC(IntPtr hwnd);
 
         [DllImport("user32.dll")]
@@ -65,6 +73,20 @@ namespace AutoClicker
 
         [DllImport("user32.dll")]
         public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
 
         public struct Rect
         {
@@ -83,6 +105,13 @@ namespace AutoClicker
 
         private const int BM_CLICK = 0x00F5;
 
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
         public enum WMessages : int
         {
             WM_LBUTTONDOWN = 0x201,
@@ -93,11 +122,19 @@ namespace AutoClicker
             WM_RBUTTONDBLCLK = 0x206
         }
 
+        private const int WH_MOUSE_LL = 14;
+
         private int MAKELPARAM(int p, int p_2)
         {
             return ((p_2 << 16) | (p & 0xFFFF));
         }
 
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        static IntPtr hHook = IntPtr.Zero;
+        public static IntPtr currentlySelectedWindow = IntPtr.Zero;
+        private static IntPtr _hookID = IntPtr.Zero;
         Thread cursorThread;
         bool threadShouldEnd = false;
 
@@ -107,25 +144,6 @@ namespace AutoClicker
 
             cursorThread = new Thread(new ThreadStart(LabelUpdate));
             cursorThread.Start();
-
-            /*
-            new Thread(() =>
-            {
-                while (true)
-                {
-                    //Logic
-                    Point p = GetMousePosition();
-
-                    //Update UI
-                    Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        mousePositionLabel.Content = p.X + ", " + p.Y;
-                    }));
-
-                    Thread.Sleep(100);
-                }
-            }).Start();
-            */
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -165,7 +183,7 @@ namespace AutoClicker
 
         private void LabelUpdate()
         {
-            while(!threadShouldEnd)
+            while (!threadShouldEnd)
             {
                 //Logic
                 Point p = GetMousePosition();
@@ -200,5 +218,59 @@ namespace AutoClicker
             return color;
         }
 
+        private void FindWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IntPtr.Zero == hHook)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    hHook = SetWindowsHookEx(WH_MOUSE_LL, _proc,
+                        GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && WMessages.WM_LBUTTONDOWN == (WMessages)wParam)
+            {
+                //  The application runs to here when you click on the window whose handle you  want to get                
+                POINT cusorPoint;
+                bool ret = GetCursorPos(out cusorPoint);
+                // cusorPoint contains your cusor’s position when you click on the window
+
+
+                // Then use cusorPoint to get the handle of the window you clicked
+
+                IntPtr winHandle = WindowFromPoint(cusorPoint);
+
+                // winHandle is the Hanle you need
+
+
+                // Now you have get the handle, do what you want here
+                // …………………………………………………. 
+
+                Console.WriteLine("Setting window to " + winHandle);
+                currentlySelectedWindow = winHandle;
+
+                // Because the hook may occupy much memory, so remember to uninstall the hook after
+                // you finish your work, and that is what the following code does.
+                UnhookWindowsHookEx(hHook);
+                hHook = IntPtr.Zero;
+
+
+                // Here I do not use the GetActiveWindow(). Let's call the window you clicked "DesWindow" and explain my reason.
+                // I think the hook intercepts the mouse click message before the mouse click message delivered to the DesWindow's 
+                // message queue. The application came to this function before the DesWindow became the active window, so the handle 
+                // abtained from calling GetActiveWindow() here is not the DesWindow's handle, I did some tests, and What I got is always 
+                // the Form's handle, but not the DesWindow's handle. You can do some test too.
+
+                //IntPtr handle = GetActiveWindow();
+
+
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
     }
 }
